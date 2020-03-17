b7b7e413d7c1bee4158c65bc3d849c5e260319b0
Nikhil Gohil <nkgohil007@gmail.com>
2020-03-17 15:39:31 +0530

    Initial Commit

---
+++ app/src/main/java/thesevenitsolutions/com/docshub/otp.java
@@ -0,0 +1,14 @@
+package thesevenitsolutions.com.docshub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class otp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp);
+    }
+}
