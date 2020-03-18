1bbe48bd51c8cce1c2a86d64e1168fc94d0800d7
Nikhil Gohil <nkgohil007@gmail.com>
2020-03-18 12:40:33 +0530

    docprofile done

---
+++ app/src/main/java/thesevenitsolutions/com/docshub/doctorprofile.java
@@ -0,0 +1,14 @@
+package thesevenitsolutions.com.docshub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class doctorprofile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctorprofile);
+    }
+}
